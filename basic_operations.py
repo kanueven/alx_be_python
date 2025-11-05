20e5ef222a8738089ab1d71a4e5b15ee4b875efa
Ray Gathoni <rachaelgathnonin@gmail.com>
2025-11-05 21:37:50 +0300

    Add ALX basic arithmetic operations

---
+++ basic_operations.py
@@ -0,0 +1,16 @@
+# ALX Assignment: Basic Arithmetic Operations
+
+number1 = 10
+number2 = 5
+
+# Perform operations
+addition_result = number1 + number2
+subtract_result = number1 - number2
+multiply_result = number1 * number2
+division_result = number1 / number2
+
+# Print results
+print("Addition of", number1, "and", number2, "is", addition_result)
+print("Subtraction of", number1, "and", number2, "is", subtract_result)
+print("Multiplication of", number1, "and", number2, "is", multiply_result)
+print("Division of", number1, "by", number2, "is:", division_result)
