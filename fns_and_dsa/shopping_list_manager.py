d80c2d543e18151aa8383dd2f5d1249704223ec6
Ray Gathoni <rachaelgathnonin@gmail.com>
2025-11-19 01:44:33 +0300

    Implement remove item feature in shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -33,7 +33,7 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter the item to add:")
+        choice = input("Enter your choice:")
         if choice == "1":
             add_item(shopping_list)
         elif choice == "2":
