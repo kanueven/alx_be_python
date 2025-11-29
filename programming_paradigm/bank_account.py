252045c50ef1ed06ea43960288fce9dd481d9d74
Gathoni <rachaelgathnonin@gmail.com>
2025-11-30 00:51:37 +0300

    Fix BankAccount display_balance to show 2 decimal places

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,7 +12,7 @@ class BankAccount:
             return True
         return False
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
     
 # my_account = BankAccount()
 # print("Initial Balance:", my_account.display_balance())
