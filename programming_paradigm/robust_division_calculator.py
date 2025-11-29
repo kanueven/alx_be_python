8da29cc299fbea22088ff19dc60fc43880a9622e
Gathoni <rachaelgathnonin@gmail.com>
2025-11-30 00:54:29 +0300

    Fix robust_division_calculator ZeroDivisionError message

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -8,12 +8,11 @@ def safe_divide(numerator, denominator):
         num = float(numerator)
         denom = float(denominator)
     except ValueError:
-        return " Error: Please enter numeric values only"
+        return "Error: Please enter numeric values only"
     
     try:
         result = num / denom
     except ZeroDivisionError:
-        return "Error: Division by zero is not allowed."
-    
+        return "Error: Cannot divide by zero."
     else:
         return f"The result of the division is {result}"
