9b4d4f548d8b1cf070d1e6eb0304738c6a0b042d
Gathoni <rachaelgathnonin@gmail.com>
2025-12-05 02:29:27 +0300

    inheritance and composition in oop

---
+++ oop/library_system/library_system.py
@@ -0,0 +1,32 @@
+# Base class
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author    
+
+#derived class
+class EBook(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+# derived class
+class PrintBook(Book):
+    def __init__(self,title,author,page_count:int):
+        super().__init__(title,author)
+        self.page_count = page_count
+# Composition class
+class Library:
+    def __init__(self):
+        self.books = []
+    
+    def add_book(self, book):
+        self.books.append(book)
+        
+    def list_books(self):
+        for book in self.books:
+            if isinstance(book, EBook):
+                print(f"E-Book: {book.title} by {book.author}, File Size: {book.file_size}MB")
+            elif isinstance(book, PrintBook):
+                print(f"Print Book: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
