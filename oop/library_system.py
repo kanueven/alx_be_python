34ca9ccca8ef473523c0e89c2614091d02a67407
Gathoni <rachaelgathnonin@gmail.com>
2025-12-05 02:40:54 +0300

    inheritance and composition in oop changes

--- oop/library_system.py
+++ oop/library_system.py
@@ -29,8 +29,8 @@ class Library:
     def list_books(self):
         for book in self.books:
             if isinstance(book, EBook):
-                print(f"E-Book: {book.title} by {book.author}, File Size: {book.file_size}KB")
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
-                print(f"Print Book: {book.title} by {book.author}, Page Count: {book.page_count}")
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
                 print(f"Book: {book.title} by {book.author}")
