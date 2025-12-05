551c37d30ecc58ced15019c7b0323f09169d96e1
Gathoni <rachaelgathnonin@gmail.com>
2025-12-05 16:09:29 +0300

    did polymorphism and class methods as well as static methods

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,19 @@
+import math
+#Base class
+class Shape:
+    def area(self):
+        pass
+    
+#Derived class
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius * self.radius
+        
